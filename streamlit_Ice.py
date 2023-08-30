81637342080d8dd5c1488d124fa164ae06c2ae64
rdboulos <89005126+rdboulos@users.noreply.github.com>
2023-08-31 09:56:48 +1000

    Update streamlit_Ice.py

--- streamlit_Ice.py
+++ streamlit_Ice.py
@@ -10,10 +10,10 @@ import os
 #from pandasai import SmartDataframe
 
 # Define the path to the folder containing the files
-folder_path = "C:/Users/Rebecca/Documents/LLMs/PandasOpenAIV2/Data"
+#folder_path = "C:/Users/Rebecca/Documents/LLMs/PandasOpenAIV2/Data"
 
 # Get a list of all files in the folder
-file_list = os.listdir(folder_path)
+#file_list = os.listdir(folder_path)
 
 # Initialize an empty list to hold data frames
 data_frames = []
@@ -22,7 +22,11 @@ data_frames = []
 for file_name in file_list:
     file_path = os.path.join(folder_path, file_name)
     if os.path.isfile(file_path):
-        df = pd.read_csv(file_path)  # You can adjust this based on the file format
+        #df = pd.read_csv(file_path)  # You can adjust this based on the file format
+
+        github_csv_url = "https://raw.githubusercontent.com/rdboulos/repository/main/icehackathon/GOTOES_FIT-CSV_Github.csv"
+        df = pd.read_csv(github_csv_url)
+
         data_frames.append(df)
 
 # Concatenate all data frames into a single data frame
